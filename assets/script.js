6c6c679f4ac469d7a6fce243ca8989adfed0a24e
Axel M <mandon.axel25@gmail.com>
2024-04-15 17:36:37 +0200

    add function banner

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,42 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+
+// Fonction pour afficher le slide actif dans les éléments existants
+function showSlideInBanner(slide) {
+    const bannerImg = document.querySelector('.banner-img');
+    const bannerText = document.querySelector('#banner p');
+	const dots = document.querySelectorAll('.dot');
+
+    bannerImg.src = slide.image;
+    bannerImg.alt = "Banner Print-it";
+    bannerText.innerHTML = slide.tagLine;
+
+	const currentIndex = slides.findIndex(s => s === slide);
+    dots.forEach((dot, index) => {
+        if (index === currentIndex) {
+            dot.classList.add('dot_selected');
+        } else {
+            dot.classList.remove('dot_selected');
+        }
+    });
+}
+
+
